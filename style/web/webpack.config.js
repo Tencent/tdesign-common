b24a2a8592e59ef2f77765565d110fc15bb2d962
chaishi <974383157@qq.com>
2020-05-18 22:14:42 +0800

    style: indetation/comma/semicolon/newline

--- style/web/webpack.config.js
+++ style/web/webpack.config.js
@@ -3,52 +3,51 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
-    mode:'development',
-    entry:'./index.js',
-    output:{
-        filename:'[name].js',
-        path:path.resolve(__dirname,'dist')
-    },
-    module:{
-        rules:[
-            {
-                test:/\.css$/,
-                use: [
-                    //MiniCssExtractPlugin.loader,
-                    'css-loader',
-                  ],
-           },{
-                test: /\.less$/,
-                use:[
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'less-loader',
-                ] // 将 Less 编译为 CSS
-            },
-        ]
-    },
-    plugins:[
-        // 单独分离css
-        new MiniCssExtractPlugin({
-            filename: 'tdesign.css',
-        }),
-        
-        // 压缩css
-        new OptimizeCssAssetsPlugin({
-            assetNameRegExp: /\.css$/g,
-            cssProcessor: require('cssnano'),
-            cssProcessorPluginOptions: {
-                preset: ['default', { discardComments: { removeAll: true } }],
-            },
-            canPrint: true
-        })
+  mode: 'development',
+  entry: './index.js',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'css-loader',
+        ],
+      }, {
+        test: /\.less$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'less-loader',
+        ],
+      },
     ],
-    devServer: {
-        contentBase:  './',
-        compress: true,
-        port: 9000,
-        open:false,
-        writeToDisk: true,
-        stats: 'errors-only',
-    },
-}
+  },
+  plugins: [
+    // 单独分离css
+    new MiniCssExtractPlugin({
+      filename: 'tdesign.css',
+    }),
+
+    // 压缩css
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+        preset: ['default', { discardComments: { removeAll: true } }],
+      },
+      canPrint: true,
+    }),
+  ],
+  devServer: {
+    contentBase: './',
+    compress: true,
+    port: 9000,
+    open: false,
+    writeToDisk: true,
+    stats: 'errors-only',
+  },
+};
