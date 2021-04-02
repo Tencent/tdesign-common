c8fd122281416f4b698bd3fe20b41f6ff21f2692
TabSpace <pillar0514@163.com>
2021-04-02 14:10:56 +0800

    test: 完善 tree 组件单元测试

--- test/unit/tree/tree-node.test.js
+++ test/unit/tree/tree-node.test.js
@@ -34,5 +34,37 @@ describe('tree-node', () => {
       expect(checked.length).toBe(1);
       expect(checked[0]).toBe('t1.1');
     });
+
+    // 手动测试中发现，选中节点意外影响了节点的高亮状态
+    // 添加此测试排除模型出错的可能性
+    it('TreeNode.setChecked will not access other property', async () => {
+      const tree = new TreeStore({
+        checkable: true,
+      });
+      tree.append([{
+        value: 't1',
+        children: [{
+          value: 't1.1',
+        }],
+      }]);
+
+      tree.setActived(['t1']);
+      await wait(1);
+
+      tree.setChecked(['t1']);
+      await wait(1);
+
+      const node1 = tree.getNode('t1');
+      const checked = tree.getChecked();
+      const actived = tree.getActived();
+
+      expect(node1.checked).toBe(true);
+      expect(checked.length).toBe(1);
+      expect(checked[0]).toBe('t1.1');
+
+      expect(node1.actived).toBe(true);
+      expect(actived.length).toBe(1);
+      expect(actived[0]).toBe('t1');
+    });
   });
 });
