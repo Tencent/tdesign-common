1f11734dd740059afc54f002c2d99d2ebbbf4721
94dreamer <503633021@qq.com>
2021-11-23 18:56:41 +0800

    fix: init .git

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    mocha: true,
+    jest: true,
+  },
+  extends: ['eslint-config-airbnb-base'],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'import',
+  ],
+  // 配置小程序内全局函数，避免报错
+  globals: {
+    require: true,
+    Page: true,
+    wx: true,
+    App: true,
+    getApp: true,
+    getCurrentPages: true,
+    Component: true,
+    getRegExp: true,
+  },
+  settings: {
+    'import/resolver': {
+      node: {},
+    },
+    'import/extensions': [
+      '.js',
+      '.jsx',
+      '.ts',
+      '.tsx',
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.config.js'],
+      rules: {
+        // node 环境下支持 require
+        '@typescript-eslint/no-require-imports': 'off',
+        'import/no-dynamic-require': 0,
+      },
+    },
+  ],
+  rules: {
+    'no-unused-vars': 0,
+    'import/extensions': 0,
+    'import/no-unresolved': 0,
+    'import/no-named-as-default': 0,
+  },
+};
