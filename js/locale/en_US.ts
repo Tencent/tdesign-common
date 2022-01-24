aee358292c1f78ef1ce2cf7a2bf6f765e2423607
HQ-Lin <a154571896@gmail.com>
2022-01-24 15:36:05 +0800

    feat: 抽离公共 locale 文件

---
+++ js/locale/en_US.ts
@@ -0,0 +1,109 @@
+// 文件有效，为国际化做准备
+export default {
+  pagination: {
+    itemsPerPage: '{size} / page',
+    jumpTo: 'jump to',
+    page: '',
+    total: '{total} items',
+  },
+  cascader: {
+    empty: 'Empty Data',
+    loadingText: 'loading...',
+    placeholder: '',
+  },
+  calendar: {
+    yearSelection: '{year}',
+    monthSelection: '{month}',
+    yearRadio: 'year',
+    monthRadio: 'month',
+    hideWeekend: 'Hide Week',
+    showWeekend: 'Show Week',
+    today: 'Today',
+    thisMonth: 'This Month',
+    week: 'Monday,Tuesday,Wedsday,Thuresday,Friday,Staturday,Sunday',
+    cellMonth: 'January,February,March,April,May,June,July,August,September,October,November,December',
+  },
+  transfer: {
+    title: '{checked} / {total}',
+    empty: 'Empty Data',
+    placeholder: 'type keyworkd to search',
+  },
+  timePicker: {
+    now: 'Now',
+    confirm: 'Confirm',
+    anteMeridiem: 'AM',
+    postMeridiem: 'PM',
+    placeholder: '',
+  },
+  dialog: {
+    confirm: 'Confirm',
+    cancel: 'Cancel',
+    confirmBtnTheme: {
+      default: 'primary',
+      info: 'primary',
+      warning: 'primary',
+      danger: 'primary',
+      success: 'primary',
+    },
+  },
+  drawer: {
+    confirm: 'Confirm',
+    cancel: 'Cancel',
+  },
+  popconfirm: {
+    confirm: 'OK',
+    cancel: 'Cancel',
+    confirmBtnTheme: {
+      default: 'primary',
+      warning: 'primary',
+      danger: 'primary',
+    },
+  },
+  table: {
+    empty: 'Empty Data',
+  },
+  select: {
+    empty: 'Empty Data',
+    loadingText: 'loading...',
+    placeholder: '',
+  },
+  tree: {
+    empty: 'Empty Data',
+  },
+  treeSelect: {
+    empty: 'Empty Data',
+    loadingText: 'loading...',
+  },
+  datePicker: {
+    placeholder: {
+      date: 'select date',
+      month: 'select month',
+      year: 'select year',
+    },
+    weekdays: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+    months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    rangeSeparator: ' ~ ',
+    format: 'YYYY-MM-DD',
+    yearAriaLabel: '',
+    confirm: 'Confirm',
+    selectTime: 'Select Time',
+    selectDate: 'Select Date',
+    nextYear: 'Next Year',
+    preYear: 'Last Year',
+    nextMonth: 'Next Month',
+    preMonth: 'Last Month',
+    preDecade: 'Last Decade',
+    nextDecade: 'Next Decade',
+    now: 'Now',
+  },
+  steps: {},
+  upload: {
+    sizeLimitMessage: 'File is too large to upload. {sizeLimit}',
+    cancelUploadText: 'Cancel',
+  },
+  tag: {},
+  form: {},
+  input: {
+    placeholder: '',
+  },
+};
