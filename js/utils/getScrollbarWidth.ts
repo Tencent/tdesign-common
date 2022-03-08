7be6c5774c3ca7962407ae574397eecc3c4fa5f0
sheepluo <sheepluo@tencent.com>
2022-03-08 18:09:35 +0800

    feat(web): 优化滚动条宽度获取

--- js/utils/getScrollbarWidth.ts
+++ js/utils/getScrollbarWidth.ts
@@ -6,7 +6,7 @@ export default function getScrollbarWidth() {
   const scrollDiv = document.createElement('div');
   scrollDiv.style.cssText = 'width: 99px; height: 99px; overflow: scroll; position: absolute; top: -9999px;';
   document.body.appendChild(scrollDiv);
-  const scrollbarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
+  const scrollbarWidth = scrollDiv.getBoundingClientRect().width - scrollDiv.clientWidth;
   document.body.removeChild(scrollDiv);
   return scrollbarWidth;
 }
