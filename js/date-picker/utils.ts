96d92516f595d7b79292e5673ad4b7303906220c
HQ-Lin <a154571896@gmail.com>
2022-03-14 13:25:20 +0800

    feat: 抽离disableDate判断

--- js/date-picker/utils.ts
+++ js/date-picker/utils.ts
@@ -1,4 +1,8 @@
+import dayjs from 'dayjs';
+import dayJsIsBetween from 'dayjs/plugin/isBetween';
 import chunk from 'lodash/chunk';
+
+dayjs.extend(dayJsIsBetween);
 
 /**
  * 首字母大写
@@ -361,4 +365,74 @@ export function extractTimeFormat(dateFormat: string) {
   const res = dateFormat.match(/(a\s)?h{1,2}:m{1,2}(:s{1,2})?(\sa)?/i);
   if (!res) return null;
   return res[0];
+}
+
+/**
+ * 禁用日期配置
+ * @param {Object} { value, disableDate, mode, format }
+ * @returns {Boolean}
+ */
+export function isEnabledDate({
+  value,
+  disableDate,
+  mode,
+  format,
+}: {
+  value: Date;
+  mode: 'year' | 'month' | 'date';
+  format: string;
+  disableDate: DisableDate;
+}): boolean {
+  if (!disableDate) return true;
+
+  let isEnabled = true;
+  // 值类型为 Function 则表示返回值为 true 的日期会被禁用
+  if (typeof disableDate === 'function') {
+    return !disableDate(value);
+  }
+
+  // 禁用日期，示例：['A', 'B'] 表示日期 A 和日期 B 会被禁用。
+  if (Array.isArray(disableDate)) {
+    let isIncludes = false;
+    const formatedDisabledDate = disableDate.map((item: string) => dayjs(item, format));
+    formatedDisabledDate.forEach((item) => {
+      if (item.isSame(dayjs(value))) {
+        isIncludes = true;
+      }
+    });
+    return !isIncludes;
+  }
+
+  // { from: 'A', to: 'B' } 表示在 A 到 B 之间的日期会被禁用。
+  const {
+    from,
+    to,
+    before,
+    after
+  } = disableDate;
+
+  if (from && to) {
+    const compareMin = dayjs(new Date(from));
+    const compareMax = dayjs(new Date(to));
+
+    return !dayjs(value).isBetween(compareMin, compareMax, mode, '[]');
+  }
+
+  const min = before ? new Date(before) : null;
+  const max = after ? new Date(after) : null;
+
+  // { before: 'A', after: 'B' } 表示在 A 之前和在 B 之后的日期都会被禁用。
+  if (max && min) {
+    const compareMin = dayjs(new Date(min));
+    const compareMax = dayjs(new Date(max));
+
+    isEnabled = dayjs(value).isBetween(compareMin, compareMax, mode, '[]');
+  } else if (min) {
+    const compareMin = dayjs(new Date(min));
+    isEnabled = !dayjs(value).isBefore(compareMin, mode);
+  } else if (max) {
+    const compareMax = dayjs(new Date(max));
+    isEnabled = !dayjs(value).isAfter(compareMax, mode);
+  }
+  return isEnabled;
 }
