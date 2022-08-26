fd14b3df18e397eecd85d3f5ffcd8c63b1cba9f0
Uyarn <uyarnchen@gmail.com>
2022-08-26 18:01:58 +0800

    fix: string and number equal

--- test/unit/input-number/compareLargeNumber.test.js
+++ test/unit/input-number/compareLargeNumber.test.js
@@ -1,68 +1,85 @@
-import { compareLargeNumber, isInputNumber, formatENumber } from '../../../js/input-number/large-number';
+import { compareNumber, isInputNumber, formatENumber } from '../../../js/input-number/large-number';
 
-describe('compareLargeNumber', () => {
+describe('compareNumber', () => {
   it('0.1, 0.2', () => {
-    expect(compareLargeNumber('0.1', '0.2')).toBe(-1);
+    expect(compareNumber('0.1', '0.2')).toBe(-1);
   });
 
   it('0.2, 0.1', () => {
-    expect(compareLargeNumber('0.2', '0.1')).toBe(1);
+    expect(compareNumber('0.2', '0.1')).toBe(1);
   });
 
   it('0.8, 0.80000', () => {
-    expect(compareLargeNumber('0.8', '0.80000')).toBe(0);
+    expect(compareNumber('0.8', '0.80000')).toBe(0);
   });
 
   it('0.88888888, 0.111', () => {
-    expect(compareLargeNumber('0.88888888', '0.111')).toBe(1);
+    expect(compareNumber('0.88888888', '0.111')).toBe(1);
   });
 
   it('1, 2', () => {
-    expect(compareLargeNumber('1', '2')).toBe(-1);
+    expect(compareNumber('1', '2')).toBe(-1);
   });
 
   it('001, 1', () => {
-    expect(compareLargeNumber('001', '1')).toBe(0);
+    expect(compareNumber('001', '1')).toBe(0);
   });
 
   it('0.01, 1', () => {
-    expect(compareLargeNumber('0.01', '1')).toBe(-1);
+    expect(compareNumber('0.01', '1')).toBe(-1);
   });
 
   it('00, 0', () => {
-    expect(compareLargeNumber('00', '0')).toBe(0);
+    expect(compareNumber('00', '0')).toBe(0);
   });
 
   it('0, 0', () => {
-    expect(compareLargeNumber('0', '0')).toBe(0);
+    expect(compareNumber('0', '0')).toBe(0);
   });
 
   it('2134, 888', () => {
-    expect(compareLargeNumber('2134', '888')).toBe(1);
+    expect(compareNumber('2134', '888')).toBe(1);
   });
 
   it('2.134, 88.8', () => {
-    expect(compareLargeNumber('2.134', '88.8')).toBe(-1);
+    expect(compareNumber('2.134', '88.8')).toBe(-1);
   });
 
   it('-2, -1', () => {
-    expect(compareLargeNumber('-2', '-1')).toBe(-1);
+    expect(compareNumber('-2', '-1')).toBe(-1);
   });
 
   it('-1, -2', () => {
-    expect(compareLargeNumber('-1', '-2')).toBe(1);
+    expect(compareNumber('-1', '-2')).toBe(1);
   });
 
   it('-2, -2', () => {
-    expect(compareLargeNumber('-2', '-2')).toBe(0);
+    expect(compareNumber('-2', '-2')).toBe(0);
   });
 
   it('4241234, 41234534', () => {
-    expect(compareLargeNumber('4241234', '41234534')).toBe(-1);
+    expect(compareNumber('4241234', '41234534')).toBe(-1);
   });
 
   it('number 2, string 2', () => {
-    expect(compareLargeNumber('2', 2)).toBe(0);
+    expect(compareNumber('2', 2)).toBe(0);
+  });
+
+  // 大数比较
+  it('string 1234567891234567890, string 1234567891234567891', () => {
+    expect(compareNumber('1234567891234567890', '1234567891234567891', true)).toBe(-1);
+  });
+
+  it('string 1234567891234567890, number 1234567891234567891', () => {
+    expect(compareNumber('1234567891234567890', 1234567891234567891, true)).toBe(-1);
+  });
+
+  it('number 1234567891234567891, string 1234567891234567890', () => {
+    expect(compareNumber(1234567891234567891, '1234567891234567890')).toBe(1);
+  });
+
+  it('number 1234567891234567891, string 1234567891234567890', () => {
+    expect(compareNumber(1234567891234567891, '1234567891234567890')).toBe(1);
   });
 });
 
