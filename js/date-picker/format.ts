b9aea8ec36472e1be1185da0995f751636aaf12d
HQ-Lin <a154571896@gmail.com>
2022-09-13 20:32:11 +0800

    feat: 默认时间调整成 00:00:00

--- js/date-picker/format.ts
+++ js/date-picker/format.ts
@@ -161,13 +161,9 @@ export function formatTime(value: DateValue | DateValue[], timeFormat: string) {
   let result;
 
   if (Array.isArray(value)) {
-    result = value.map((v) =>
-      dayjs(v || new Date(new Date().setHours(0, 0, 0, 0))).format(timeFormat)
-    );
+    result = value.map((v) => dayjs(v || new Date(new Date().setHours(0, 0, 0, 0))).format(timeFormat));
   } else {
-    result = dayjs(
-      (value || new Date(new Date().setHours(0, 0, 0, 0))) as DateValue
-    ).format(timeFormat);
+    result = dayjs((value || new Date(new Date().setHours(0, 0, 0, 0)))).format(timeFormat);
   }
 
   return result;
