8fe89fc7d4b223c8768f3856b7d1c829bf947b20
Caster <moecasts.caster@gmail.com>
2022-12-26 19:04:20 +0800

    feat(tooltip-lite): mouse 模式下新增定位边界检查，保证内容在可视范围内

--- js/utils/getPosition.ts
+++ js/utils/getPosition.ts
@@ -32,7 +32,27 @@ const getPosition = (
         break;
       // 后续有需要可以再扩展
     }
+
+    if (placement === 'mouse') {
+      const edges = {
+        top: document.documentElement.scrollTop,
+        left: document.documentElement.scrollLeft,
+        right: document.documentElement.scrollLeft + document.documentElement.clientWidth,
+        bottom: document.documentElement.scrollTop + document.documentElement.clientHeight,
+      };
+
+      // 底部溢出时，定位到元素上方
+      if (position.top > edges.bottom - contentRect.height) {
+        position.top = document.documentElement.scrollTop + targetRect.top - contentRect.height - 8;
+      }
+
+      // 右侧溢出时，确保元素能完全展示
+      if (position.left > edges.right - contentRect.width) {
+        position.left = edges.right - contentRect.width;
+      }
+    }
   }
+
   return position;
 };
 
