319b72551061e3ccfab10e929d1f75730ef8a6a8
李江辰 <SpaceCode@foxmail.com>
2023-11-10 11:24:24 +0800

    feat(statistic): abstracted common code from the statistic component

---
+++ js/statistic/utils.ts
@@ -0,0 +1,9 @@
+/**
+ * 颜色映射
+ */
+export const COLOR_MAP = {
+  blue: 'var(--td-brand-color)',
+  red: 'var(--td-error-color)',
+  orange: 'var(--td-warning-color)',
+  green: 'var(--td-success-color)',
+};
