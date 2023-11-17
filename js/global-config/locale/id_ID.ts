0973e0c5558b593e0eaeadaf67d2169783a8e32e
叶松滔 <13560556691>
2023-11-17 09:51:36 +0800

    feat(locale): 新增印尼语国际化语言支持

---
+++ js/global-config/locale/id_ID.ts
@@ -0,0 +1,230 @@
+/* eslint-disable no-template-curly-in-string */
+// 印尼语-印尼
+import 'dayjs/locale/id';
+
+export default {
+  pagination: {
+    itemsPerPage: '{size} bilah/helai',
+    jumpTo: 'melompat ke',
+    page: 'helai',
+    total: 'jumlah {total} data',
+  },
+  cascader: {
+    empty: 'kosong',
+    loadingText: 'memuat',
+    placeholder: 'silakan pilih',
+  },
+  calendar: {
+    yearSelection: '{year}',
+    monthSelection: '{month}',
+    yearRadio: 'tahun',
+    monthRadio: 'bulan',
+    hideWeekend: 'sembunyikan minggu',
+    showWeekend: 'tamplikan minggu',
+    today: 'hari ini',
+    thisMonth: 'bulan ini',
+    week: 'Senin,Selasa,Rabu,Kamis,Jumat,Sabtu,Minggu',
+    cellMonth:
+      'Januari,Februari,Maret,April,May,June,July,August,September,October,November,December',
+  },
+  transfer: {
+    title: '{checked} / {total}',
+    empty: 'kosong',
+    placeholder: 'silakan masukkan kata kunci untuk cari',
+  },
+  timePicker: {
+    dayjsLocale: 'id',
+    now: 'sekarang',
+    confirm: 'mengesahkan',
+    anteMeridiem: 'pagi',
+    postMeridiem: 'petang',
+    placeholder: 'pilih masa',
+  },
+  dialog: {
+    confirm: 'mengesahkan',
+    cancel: 'batalkan',
+  },
+  drawer: {
+    confirm: 'mengesahkan',
+    cancel: 'batalkan',
+  },
+  popconfirm: {
+    confirm: {
+      content: 'memastikan',
+    },
+    cancel: {
+      content: 'batalkan',
+    },
+  },
+  table: {
+    empty: 'kosong',
+    loadingText: 'memuat, silakan menunggu',
+    loadingMoreText: 'memuat lebih',
+    filterInputPlaceholder: 'silakan masukkan kandungan',
+    sortAscendingOperationText: 'urus operasi naik',
+    sortCancelOperationText: 'urus operasi batal',
+    sortDescendingOperationText: 'usrus operasi turun',
+    clearFilterResultButtonText: 'hapus penyaring',
+    columnConfigButtonText: 'konfigurasi kolom',
+    columnConfigTitleText: 'konfigurasi kolom tabel',
+    columnConfigDescriptionText:
+      'Silakan pilih kolom data yang ingin ditampilkan pada tabel',
+    confirmText: 'mengesahkan',
+    cancelText: 'batalkan',
+    resetText: 'mengatur ulang',
+    selectAllText: 'pilih juamlah',
+    searchResultText: 'mencari"{result}",{count} hasil',
+  },
+  select: {
+    empty: 'kosong',
+    loadingText: 'memuat',
+    placeholder: 'silakan pilih',
+  },
+  tree: {
+    empty: 'kosong',
+  },
+  treeSelect: {
+    empty: 'kosong',
+    loadingText: 'memuat',
+    placeholder: 'silakan pilih',
+  },
+  datePicker: {
+    dayjsLocale: 'id',
+    placeholder: {
+      date: 'silakan pilih tanggal',
+      month: 'silakann pilih bulan',
+      year: 'silakan pilih tahun',
+    },
+    weekdays: ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu'],
+    months: [
+      'Januari',
+      'Februari',
+      'Maret',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ],
+    quarters: ['suku pertama', 'suku kedua', 'suku ketiga', 'suku keempat'],
+    rangeSeparator: ' - ',
+    direction: 'ltr',
+    format: 'YYYY-MM-DD',
+    dayAriaLabel: 'hari',
+    weekAbbreviation: 'minggu',
+    yearAriaLabel: 'tahun',
+    monthAriaLabel: 'bulan',
+    confirm: 'mengesahkan',
+    selectTime: 'pilih masa',
+    selectDate: 'pilih tanggal',
+    nextYear: 'tahun depan',
+    preYear: 'tahu lalu',
+    nextMonth: 'bulan tepan',
+    preMonth: 'bulan lalu',
+    preDecade: 'dekad depan',
+    nextDecade: 'dekad lalu',
+    now: 'sekarang',
+  },
+  upload: {
+    sizeLimitMessage: 'had saiz dokumen {sizeLimit}',
+    cancelUploadText: 'batalkan muat naik',
+    triggerUploadText: {
+      fileInput: 'pilih dokumen',
+      image: 'klik untuk muat naik gambar',
+      normal: 'klik untuk muat naik',
+      reupload: 'pilih ulang lagi',
+      continueUpload: 'pilih terus',
+      delete: 'memotong',
+      uploading: 'muat naik',
+    },
+    dragger: {
+      dragDropText: 'lepaskan tikus',
+      draggingText: 'heret ke kawasan ini',
+      clickAndDragText:
+        'klik "pilih dokumen" atas atau heret dokumen ke kawasan ini',
+    },
+    file: {
+      fileNameText: 'nama dokumen',
+      fileSizeText: 'saiz dokumen',
+      fileStatusText: 'keadaan',
+      fileOperationText: 'operasi',
+      fileOperationDateText: 'tarikh muat naik',
+    },
+    progress: {
+      uploadingText: 'mengunggah',
+      waitingText: 'untuk diunggah',
+      failText: 'gagal mengunggah',
+      successText: 'berhasil diunggah',
+    },
+  },
+  form: {
+    errorMessage: {
+      date: 'silakan masukkan ${name} betul',
+      url: 'silakan masukkan ${name} betul',
+      required: '${name} diperuhkan',
+      max: 'panjang karater ${name} tidak boleh melebihi panajng karater ${validate}, satu karakter Cina sama dengan dua karakter',
+      min: 'panjang karater ${name} tidak boleh kurang dari panajng karater ${validate}, satu karakter Cina sama dengan dua karakter',
+      len: 'panjang karater ${name} harus adalah ${validate}',
+      enum: '${name} harus adalah ${validate} dan lain-alin',
+      idcard: 'silakan masukkan ${name} betul',
+      telnumber: 'silakan masukkan ${name} betul',
+      pattern: 'silakan masukkan ${name} betul',
+      validator: '${name} tidak tidak patuh',
+      boolean: ' jelnis data ${name} harus adalah jenis boolean',
+      number: '${name} harus nomor',
+    },
+  },
+  input: {
+    placeholder: 'silakan masukkan',
+  },
+  list: {
+    loadingText: 'memuat sekarang, silakan menunggu',
+    loadingMoreText: 'klik memuat lebih',
+  },
+  alert: {
+    expandText: 'memperluas',
+    collapseText: 'menutup',
+  },
+  anchor: {
+    copySuccessText: 'salin link berhasil',
+    copyText: 'salin link',
+  },
+  colorPicker: {
+    swatchColorTitle: 'warna contoh sistem',
+    recentColorTitle: 'warna guna resen',
+    clearConfirmText:
+      'Apakah Anda yakin ingin menghapus warna yang baru saja digunakan?',
+  },
+  guide: {
+    finishButtonProps: {
+      content: 'habis',
+      theme: 'primary',
+    },
+    nextButtonProps: {
+      content: 'berikutnya',
+      theme: 'primary',
+    },
+    skipButtonProps: {
+      content: 'lompat',
+      theme: 'default',
+    },
+    prevButtonProps: {
+      content: 'sebelumnya',
+      theme: 'default',
+    },
+  },
+  image: {
+    errorText: 'gambar salah',
+    loadingText: 'gambar sedang memuat',
+  },
+  imageViewer: {
+    errorText: 'gambar memuat dengan salah,boleh cuba memuat lagi',
+    mirrorTipText: 'cermin',
+    rotateTipText: 'putar',
+    originalSizeTipText: 'saiz asal',
+  },
+} as const;
