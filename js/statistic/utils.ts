ba5ef16a57c3b748e0263304c7a14d818f6de16d
黎伟杰 <674416404@qq.com>
2023-11-27 21:31:38 +0800

    feat(statistic): add getFormatValue

--- js/statistic/utils.ts
+++ js/statistic/utils.ts
@@ -7,3 +7,13 @@ export const COLOR_MAP = {
   orange: 'var(--td-warning-color)',
   green: 'var(--td-success-color)',
 };
+
+export function getFormatValue(value:number | undefined | string, decimalPlaces:number, separator:string) {
+  const options = {
+    minimumFractionDigits: decimalPlaces ?? 0,
+    maximumFractionDigits: decimalPlaces ?? 20,
+    useGrouping: !!separator,
+  };
+  // replace的替换的方案仅能应对大部分地区
+  return value.toLocaleString(undefined, options).replace(/,|，/g, separator);
+}
