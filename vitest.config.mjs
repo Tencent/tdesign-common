ec80365f1949cfdd3fae4904b4f2b197499b7c46
黎伟杰 <674416404@qq.com>
2024-12-30 21:04:11 +0800

    chore: update test config

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    environment: 'jsdom',
     include: ['test/unit/**/*test.{ts,js}'],
     coverage: {
       reportsDirectory: './test/coverage',
