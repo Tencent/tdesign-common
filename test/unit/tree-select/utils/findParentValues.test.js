0764d9e18f316bdfe97f3509ffbbc9ed1c9d59ef
黎伟杰 <674416404@qq.com>
2025-02-12 10:03:42 +0800

    chore: add mote tests

--- test/unit/tree-select/utils/findParentValues.test.js
+++ test/unit/tree-select/utils/findParentValues.test.js
@@ -27,6 +27,18 @@ describe('tree-select:findParentValues', () => {
       const value = 't1.1.1';
       const parentValues = findParentValues(data, value, 'value', 'children');
       expect(parentValues).toEqual(['t1', 't1.1']);
+
+      const value2 = 't2.1';
+      const parentValues2 = findParentValues(data, value2, 'value', 'children');
+      expect(parentValues2).toEqual(['t2']);
+
+      const value3 = 't2';
+      const parentValues3 = findParentValues(data, value3, 'value', 'children');
+      expect(parentValues3).toEqual([]);
+
+      const value4 = 't3';
+      const parentValues4 = findParentValues(data, value4, 'value', 'children');
+      expect(parentValues4).toEqual([]);
     });
 
     it('object', () => {
@@ -34,6 +46,18 @@ describe('tree-select:findParentValues', () => {
       const parentValues = findParentValues(data, value, 'value', 'children');
       expect(parentValues).toEqual(['t1', 't1.1']);
     });
+
+    const value2 = { value: 't2.1', label: 't2.1' };
+    const parentValues2 = findParentValues(data, value2, 'value', 'children');
+    expect(parentValues2).toEqual(['t2']);
+
+    const value3 = { value: 't2', label: 't2' };
+    const parentValues3 = findParentValues(data, value3, 'value', 'children');
+    expect(parentValues3).toEqual([]);
+
+    const value4 = { value: 't3', label: 't3' };
+    const parentValues4 = findParentValues(data, value4, 'value', 'children');
+    expect(parentValues4).toEqual([]);
   });
 
   describe('alias', () => {
@@ -61,12 +85,36 @@ describe('tree-select:findParentValues', () => {
       const value = 't1.1.1';
       const parentValues = findParentValues(aliasData, value, 'key', 'group');
       expect(parentValues).toEqual(['t1', 't1.1']);
+
+      const value2 = 't2.1';
+      const parentValues2 = findParentValues(aliasData, value2, 'key', 'group');
+      expect(parentValues2).toEqual(['t2']);
+
+      const value3 = 't2';
+      const parentValues3 = findParentValues(aliasData, value3, 'key', 'group');
+      expect(parentValues3).toEqual([]);
+
+      const value4 = 't3';
+      const parentValues4 = findParentValues(aliasData, value4, 'key', 'group');
+      expect(parentValues4).toEqual([]);
     });
 
     it('object', () => {
       const value = { key: 't1.1.1', name: 't1.1.1' };
       const parentValues = findParentValues(aliasData, value, 'key', 'group');
       expect(parentValues).toEqual(['t1', 't1.1']);
+
+      const value2 = { key: 't2.1', name: 't2.1' };
+      const parentValues2 = findParentValues(aliasData, value2, 'key', 'group');
+      expect(parentValues2).toEqual(['t2']);
+
+      const value3 = { key: 't2', name: 't2' };
+      const parentValues3 = findParentValues(aliasData, value3, 'key', 'group');
+      expect(parentValues3).toEqual([]);
+
+      const value4 = { key: 't3', name: 't3' };
+      const parentValues4 = findParentValues(aliasData, value4, 'key', 'group');
+      expect(parentValues4).toEqual([]);
     });
   });
 });
