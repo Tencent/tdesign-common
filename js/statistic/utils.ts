9dfccad16f7d1d2488a97bb077ab48d38b1637a1
RSS1102 <jimmyrss1102@gmail.com>
2025-08-04 16:02:27 +0800

    chore: remove unused `COLOR_MAP` constants

--- js/statistic/utils.ts
+++ js/statistic/utils.ts
@@ -1,14 +1,3 @@
-/**
- * 颜色映射
- */
-export const COLOR_MAP = {
-  black: 'var(--td-text-color-primary)',
-  blue: 'var(--td-brand-color)',
-  red: 'var(--td-error-color)',
-  orange: 'var(--td-warning-color)',
-  green: 'var(--td-success-color)',
-};
-
 export function getFormatValue(value:number | undefined | string, decimalPlaces:number, separator:string) {
   const options = {
     minimumFractionDigits: decimalPlaces ?? 0,
