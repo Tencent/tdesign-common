2e2f3bfcc1a907c9eb98bbcefbfe52d1ca7d4664
zhangpaopao <zhangpaopao0609@gmail.com>
2025-12-31 15:49:54 +0800

    refactor: common

--- js/common/index.ts
+++ js/common/index.ts
@@ -1,32 +1,10 @@
 export * from './types';
 export * from './constants';
 export * from './components';
-export {
-  observe,
-  setStyle,
-  injectStyle,
-  getScrollbarWidth,
-  getScrollbarWidthWithCSS,
-  getIEVersion,
-  getFlexGapPolyFill,
-  calculateNodeSize,
-  calcTextareaHeight,
-  getColorTokenColor,
-} from './dom';
-export { linear, easeInOutCubic } from './number';
-export type { EasingFunction } from './number';
-export { template, firstUpperCase, getCharacterLength, getUnicodeLength, limitUnicodeMaxLength } from './string';
-export {
-  hasOwn,
-  getPropertyValFromObj,
-  isPlainObject,
-  isPromise,
-  omit,
-  getValidAttrs,
-  removeEmptyAttrs,
-  getTabElementByValue,
-} from './object';
-export { swapDragArrayElement } from './array';
-export { getBackgroundColor, pxCompat } from './style';
-export type { Gradients, FromTo, LinearGradient } from './style';
-export { getPosition, calcSize } from './layout';
+export * from './dom';
+export * from './number';
+export * from './string';
+export * from './object';
+export * from './array';
+export * from './style';
+export * from './layout';
